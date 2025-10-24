e1b778b09dbe3fcb9e018ca3228edcf724bdfdae
Jonata Henrique <jonatah.1512@gmail.com>
2025-10-24 17:39:14 -0300

    Exercicio 4 API

---
+++ 2º Sem/JS/exeAPI_4/script.js
@@ -0,0 +1,26 @@
+async function start() {
+    const filmes2025 = ['One Battle After Another', 'Sinners', 'Hamnet', 
+        'Sentimental Value', 'Bugonia', 'Thunderbolts', 'No Other Choice', 'Together',
+        'Weapons', 'F1', 'Mickey 17', 'Materialists']
+        const apikey = '&apikey=82281742'
+    try {
+        const mural = document.getElementById("filmes2025")
+        mural.innerText = ""
+        for(let n of filmes2025){ 
+            const tituloRequest = n.split(" ").join("+")
+            const resposta = await fetch(`http://www.omdbapi.com/?t=${tituloRequest}${apikey}`)
+            const dados = await resposta.json()
+            const titulo = dados.Title
+            const linkPoster = dados.Poster
+            const div = document.createElement("div")
+            const p = document.createElement("p")
+            const img = document.createElement("img")
+            img.src = linkPoster; p.textContent = titulo
+            div.append(img,p); div.className = "posterFilme"
+            mural.appendChild(div)
+        }
+    } catch (erro){
+        console.log(`Erro: ${erro}`)
+        alert("Não foi possível encontrar.")
+    }
+}

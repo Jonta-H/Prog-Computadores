7bba5002b4b6d20b1a01e08a81800a746989d81e
JONATA HENRIQUE DA SILVA CARÁ <26382@unifacef.edu.br>
2025-10-24 22:06:41 -0300

    att

--- 2º Sem/JS/exeAPI_4/script.js
+++ 2º Sem/JS/exeAPI_4/script.js
@@ -1,12 +1,12 @@
 async function start() {
-    const filmes2025 = ['One Battle After Another', 'Sinners', 'Hamnet', 
+    const filmes2025 = ['One Battle After Another', 'Sinners', 'Hamnet',
         'Sentimental Value', 'Bugonia', 'Thunderbolts', 'No Other Choice', 'Together',
         'Weapons', 'F1', 'Mickey 17', 'Materialists']
-        const apikey = '&apikey=82281742'
+    const apikey = '&apikey=82281742'
     try {
         const mural = document.getElementById("filmes2025")
         mural.innerText = ""
-        for(let n of filmes2025){ 
+        for (let n of filmes2025) {
             const tituloRequest = n.split(" ").join("+")
             const resposta = await fetch(`http://www.omdbapi.com/?t=${tituloRequest}${apikey}`)
             const dados = await resposta.json()
@@ -16,11 +16,25 @@ async function start() {
             const p = document.createElement("p")
             const img = document.createElement("img")
             img.src = linkPoster; p.textContent = titulo
-            div.append(img,p); div.className = "posterFilme"
+            div.append(img, p); div.className = "posterFilme"
             mural.appendChild(div)
         }
-    } catch (erro){
+    } catch (erro) {
         console.log(`Erro: ${erro}`)
         alert("Não foi possível encontrar.")
+    }
+}
+
+async function pesquisar() {
+    const apikey = '&apikey=82281742'
+    const container = document.getElementsByClassName("container")
+    const input = document.getElementById("inDados").value
+    try {
+        const tituloRequest = input.split(" ").join("+")
+        const resposta = await fetch(`http://www.omdbapi.com/?t=${tituloRequest}${apikey}`)
+        const dados = await resposta.json()
+        console.log(`${input}\n${tituloRequest}`)
+    } catch (erro) {
+        console.log(`Erro: ${erro}`)
     }
 }
